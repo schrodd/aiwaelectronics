475d03004d795bcae0b2492960f5281eebc0e0a5
amolina-aiwa <amolina@totalhome.com.ar>
2022-09-05 16:34:58 -0300

    unknown property bug

--- components/Head/index.js
+++ components/Head/index.js
@@ -18,7 +18,7 @@ export default function HeadThings(){
       <meta name="description" content="Encontrá los mejores Parlantes y Torres de Sonido más potentes del mercado. ¡Armá tu fiesta!"/>
       <meta name="keywords" content="parlante, sonido, aiwa, aiwa electronics, torre de sonido, woofer, tweeter"/>
       <meta name="robots" content="index, follow"/>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
       <meta name="language" content="English"/>
       
     </Head>
