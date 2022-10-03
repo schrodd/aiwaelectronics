43c5dfc4f7ee87ca98db7aa9454520f523825991
amolina-aiwa <amolina@totalhome.com.ar>
2022-10-03 15:13:35 -0300

    added 360 page

---
+++ pages/360/index.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Image from 'next/image'
+import { Slider } from '@mui/material'
+
+export default function threeSixty(){
+  const [image, setImage] = useState('/products/AW-T2008/360/1.webp')
+  function handleChange(v) {
+    setImage(`/products/AW-T2008/360/${v}.webp`)
+  }
+  return (
+    <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", padding: "50px"}}>
+      <Image priority src={image} width='1000px' height='1000px'></Image>
+      <Slider
+        style={{width: "200px"}}
+        aria-label="Image"
+        defaultValue={1}
+        valueLabelDisplay="auto"
+        min={1}
+        max={40}
+        onChange={(e,v) => handleChange(v)}
+      />
+    </div>
+  )
+}
