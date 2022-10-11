909e762b8acecf415b1ed3ad6b10e3f8c31c2375
amolina-aiwa <amolina@totalhome.com.ar>
2022-10-11 15:53:27 -0300

    navbar edit final

--- components/NavItem/index.js
+++ components/NavItem/index.js
@@ -4,8 +4,10 @@ export default function NavItem({menu, activeTab, setActive, closeModal}){
   function toggleHover(){
     if (activeTab === menu.id) {
       setActive(0)
+      console.log("a")
     } else {
       setActive(menu.id)
+      console.log("b")
     }
     if (!menu.content) {
       setActive(0)
@@ -19,9 +21,9 @@ export default function NavItem({menu, activeTab, setActive, closeModal}){
     <li className={`${menu.name.toLowerCase()}`}>
       {menu.isPopup
       ? <button onMouseEnter={(toggleHover)} onClick={toggleHover}>{menu.name.toUpperCase()}</button>
-      : <button><Link href={menu.link}><a onMouseEnter={(toggleHover)}>{menu.name.toUpperCase()}</a></Link></button>}
-      {activeTab === menu.id && 
-      <div className='nav-hover' onMouseLeave={toggleHover}>
+      : <button><Link href={menu.link}><a onMouseEnter={(toggleHoverModal)}>{menu.name.toUpperCase()}</a></Link></button>}
+      {menu.content &&
+      <div className={`nav-hover ${activeTab === menu.id && "active"}`}>
         {menu.content.map((e,i) => <Link key={i} href={e.link}><a onClick={closeModal ? toggleHoverModal : toggleHover}>{e.name}</a></Link>)}
       </div>}
     </li>
