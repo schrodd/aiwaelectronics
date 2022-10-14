9a00564f49aa5ea3f5e2b271e40cceaaa69af1c7
amolina-aiwa <amolina@totalhome.com.ar>
2022-10-14 15:15:44 -0300

    added auto for 360 container

--- components/ThreeSixty/index.js
+++ components/ThreeSixty/index.js
@@ -6,7 +6,11 @@ import ReactHammer from 'react-hammerjs'
 export default function ThreeSixty(){
   const [frac, setFrac] = useState(1000)
   const [idx, setIdx] = useState(1)
+  const [auto, setAuto] = useState(false)
+  const [intval, setIntval] = useState(0)
+  let tempIntval = 0
   const sens = 250
+  const spd = 100
   useEffect(() => {
     setIdx(Math.ceil(frac/1000))
   }, [frac])
@@ -16,23 +20,37 @@ export default function ThreeSixty(){
   function handlePan(e){
     if (e.direction == 2) {
       if (idx < 40) {
-        setFrac(frac+sens)
+        setFrac(frac + sens)
       } else if (idx == 40) {
         setFrac(1000)
       }
     } else if (e.direction == 4) {
       if (idx > 1) {
-        setFrac(frac-sens)
+        setFrac(frac - sens)
       } else if (idx == 1) {
         setFrac(40000)
       }
     }
   }
+  function handleAuto(){
+    if (!auto) {
+      setAuto(true)
+      tempIntval = setInterval(() => {
+        console.log(intval)
+        setFrac(prev => prev <= 39500 ? prev + 1000 : 1000)
+      }, spd)
+      setIntval(tempIntval)
+    }
+  }
+  function clearIntval(){
+    clearInterval(intval)
+    setAuto(false)
+  }
 
   return (
     <div
     style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", border: "3px solid red", margin: "10px", position: "relative"}}>
-      <ReactHammer onPan={e => handlePan(e)}>
+      <ReactHammer onPan={e => handlePan(e)} onTap={handleAuto}>
         <div>
           <Image className='not-draggable' priority src={`/products/AW-T2008/360/${idx}.webp`} width='500px' height='500px' layout='fixed'></Image>
         </div>
@@ -47,7 +65,8 @@ export default function ThreeSixty(){
         value={idx}
         onChange={(e,v) => handleChange(v)}
       />
-      <button onClick={()=>start()}></button>
+      <button onClick={handleAuto} style={{marginBottom: "20px", padding: "3px 8px"}}>Play</button>
+      <button onClick={clearIntval} style={{marginBottom: "20px", padding: "3px 8px"}}>Stop</button>
     </div>
   )
 }
