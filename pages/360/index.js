055137632462c1f739cae7eeaeda1fe64cabfd7e
amolina-aiwa <amolina@totalhome.com.ar>
2022-10-20 10:20:23 -0300

    added meta tag to avoid zoom on the page

--- pages/360/index.js
+++ pages/360/index.js
@@ -1,10 +1,14 @@
+import Head from "next/head"
 import ThreeSixty from "../../components/ThreeSixty"
 
 export default function threeSixty(){
   return (
     <>
-    <ThreeSixty sku='AW-T2008' />
-    <ThreeSixty sku='AW-T2010' />
+      <Head>
+        <meta name = "viewport" content = "width=device-width, minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no" />
+      </Head>
+      <ThreeSixty sku='AW-T2008' />
+      <ThreeSixty sku='AW-T2010' />
     </>
   )
 }
