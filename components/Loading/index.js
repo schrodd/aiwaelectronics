2ae7ef829ff39b867aa347b1104f7e68eb752fe2
amolina-aiwa <amolina@totalhome.com.ar>
2022-10-26 12:55:54 -0300

    load screen

--- components/Loading/index.js
+++ components/Loading/index.js
@@ -1,7 +1,7 @@
 export default function Loading(){
   return (
     <div className='spinner-wrapper'>
-      <div class="lds-facebook"><div></div><div></div><div></div></div>
+      <div className="lds-facebook"><div></div><div></div><div></div></div>
     </div>
   )
 }
--- pages/productos/[prod].js
+++ pages/productos/[prod].js
@@ -5,7 +5,6 @@ import ImageIcon from '@mui/icons-material/Image'
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import Link from "next/link"
-import Head from "next/head"
 
 export default function Producto(){
   const url = useRouter()
@@ -13,9 +12,6 @@ export default function Producto(){
   const selectedProduct = products.find(e => e.sku === prod)
   return (
     <section className='product-page'>
-      <Head>
-        <title>{selectedProduct && selectedProduct.sku} | AIWA Electronics</title>
-      </Head>
       {!selectedProduct ? <Loading/>
       :(<div className='top'>
           <div className='product-description'>
