614ec090dbfd12a388480fa545fa1b5fddb6acb2
amolina-aiwa <amolina@totalhome.com.ar>
2022-11-11 11:41:03 -0300

    moved 360 images to external location

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'aiwaelectronics.com.ar',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
